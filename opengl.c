284cc6887c79c02bdb30dc561b0a4ee8840acbf2
sunyijie1314 <1052112715@qq.com>
2021-05-11 09:58:51 +0800

    opengl

---
+++ opengl.c
@@ -0,0 +1,59 @@
+nclude <GLES2/gl2.h>
+
+// #include <EGL/egl.h>
+//
+// // #include <EGL/eglext.h>
+//
+// #include <emscripten.h>
+//
+// // #include <emscripten/bind.h>
+//
+// // #include <emscripten/val.h>
+//
+// #include <emscripten/html5.h>
+//  
+//
+//  // #include  <X11/Xlib.h>
+//
+//  // #include  <X11/Xatom.h>
+//
+//  // #include  <X11/Xutil.h>
+//   
+//
+//   #include <string.h>
+//
+//   #include <stdio.h>
+//
+//   #include <iostream>
+//
+//   #include <assert.h>
+//    
+//
+//    #define SR_OK 0;
+//    #define SR_FAIL 1;
+//     
+//
+//     #define Y 0
+//
+//     #define U 1
+//
+//     #define V 2
+//      
+//
+//
+//      /// esCreateWindow flag - RGB color buffer
+//
+//      #define ES_WINDOW_RGB           0
+//
+//      /// esCreateWindow flag - ALPHA color buffer
+//
+//      #define ES_WINDOW_ALPHA         1 
+//
+//      /// esCreateWindow flag - depth buffer
+//
+//      #define ES_WINDOW_DEPTH         2 
+//
+//      /// esCreateWindow flag - stencil buffer
+//
+//      #define ES_WINDOW_STENCIL       4
+//
