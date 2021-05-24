c0c013c27e2ab2a49e5477cf6dbc035a61a06951
sunyijie1314 <1052112715@qq.com>
2021-05-24 18:58:03 +0800

    update

--- decode.c
+++ decode.c
@@ -702,7 +702,7 @@ EM_BOOL parse(const EmscriptenWebSocketMessageEvent *websocketEvent)
 		}
 		start1 = 1;
 		num =0;
-		memset(oneframe, 0, 1000000);
+		memset(oneframe, 0, 300000);
 		for(int i =0; i< websocketEvent->numBytes; i++)
 		{
 			if((*(websocketEvent->data + 12 +i) == 0x00) &&
@@ -821,10 +821,10 @@ EMSCRIPTEN_KEEPALIVE
 int webassembly()
 {
 	openDecoder(0, kLogLevel_Core);
-        oneframe = (uint8_t *)malloc(1000000);
-	memset(oneframe, 0, 1000000);
-	oneframe1 = (uint8_t *)malloc(1000000);
-	memset(oneframe1, 0, 1000000);
+        oneframe = (uint8_t *)malloc(300000);
+	memset(oneframe, 0, 300000);
+	//oneframe1 = (uint8_t *)malloc(1000000);
+	//memset(oneframe1, 0, 1000000);
 	emscripten_sleep(1000);
         if(!emscripten_websocket_is_supported())
 	{
